0b3dbc4e2b6e369d07781cdb8c422d2d5a2107c3
Andrei Tserakhau <andrei_tserakhau@epam.com>
2016-08-26 14:58:54 +0300

    Initial version

---
+++ scripts/main.js
@@ -0,0 +1,1 @@
+"use strict";$(".skillbar").each(function(e,n){n.childNodes[1].style.width=n.childNodes[3].innerHTML}),$(".nav__header").click(function(){$(".nav__header").toggleClass("nav__header--open ")});
