e9c0e0937cf85ce95c9d6a580b35a3f7578ecd6e
devochka-valera <valeria.ryabenko@gmail.com>
2016-08-31 00:02:19 +0300

    fixed incorrect jQery about skill_percent

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,1 @@
-"use strict";$(".skillbar").each(function(e,n){n.childNodes[1].style.width=n.childNodes[3].innerHTML}),$(".nav__header").click(function(){$(".nav__header").toggleClass("nav__header--open")});
+"use strict";$(".skillbar").each(function(e,l){$(l).children(".skillbar__line").width($(l).children(".skillbar__percent").html())}),$(".nav__header").click(function(){$(".nav__header").toggleClass("nav__header--open")});
